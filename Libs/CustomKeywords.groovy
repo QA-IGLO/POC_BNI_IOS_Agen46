2b92e340e2a81b59fcccdd051203df6295b29c97
QA-IGLO <sylvia.astrida@indocyber.co.id>
2024-06-07 14:01:04 +0700

    upload Libs

---
+++ Libs/CustomKeywords.groovy
@@ -0,0 +1,658 @@
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+
+import java.lang.String
+
+import org.apache.poi.ss.usermodel.Workbook
+
+import java.util.List
+
+import org.apache.poi.ss.usermodel.Sheet
+
+import java.lang.Object
+
+import java.util.Map
+
+import org.apache.poi.ss.usermodel.Cell
+
+import org.apache.poi.ss.usermodel.Row
+
+
+
+def static "BlockService.insertTiketInquiry"(
+    	String databaseName	
+     , 	String collectionName	) {
+    (new BlockService()).insertTiketInquiry(
+        	databaseName
+         , 	collectionName)
+}
+
+
+def static "BlockService.deleteTiketInquiry"(
+    	String databaseName	
+     , 	String collectionName	) {
+    (new BlockService()).deleteTiketInquiry(
+        	databaseName
+         , 	collectionName)
+}
+
+
+def static "BlockService.insertSetorTunaiInquiry"(
+    	String databaseName	
+     , 	String collectionName	) {
+    (new BlockService()).insertSetorTunaiInquiry(
+        	databaseName
+         , 	collectionName)
+}
+
+
+def static "BlockService.deleteSetorTunaiInquiry"(
+    	String databaseName	
+     , 	String collectionName	) {
+    (new BlockService()).deleteSetorTunaiInquiry(
+        	databaseName
+         , 	collectionName)
+}
+
+
+def static "BlockService.insertTransferBNIInquiry"(
+    	String databaseName	
+     , 	String collectionName	) {
+    (new BlockService()).insertTransferBNIInquiry(
+        	databaseName
+         , 	collectionName)
+}
+
+
+def static "BlockService.deleteTransferBNIInquiry"(
+    	String databaseName	
+     , 	String collectionName	) {
+    (new BlockService()).deleteTransferBNIInquiry(
+        	databaseName
+         , 	collectionName)
+}
+
+
+def static "BlockService.insertTfInquiry"(
+    	String databaseName	
+     , 	String collectionName	) {
+    (new BlockService()).insertTfInquiry(
+        	databaseName
+         , 	collectionName)
+}
+
+
+def static "BlockService.deleteTfInquiry"(
+    	String databaseName	
+     , 	String collectionName	) {
+    (new BlockService()).deleteTfInquiry(
+        	databaseName
+         , 	collectionName)
+}
+
+
+def static "BlockService.insertServiceBlock"(
+    	String nameServiceBlock	) {
+    (new BlockService()).insertServiceBlock(
+        	nameServiceBlock)
+}
+
+
+def static "BlockService.deleteServiceBlock"(
+    	String nameServiceBlock	) {
+    (new BlockService()).deleteServiceBlock(
+        	nameServiceBlock)
+}
+
+
+def static "MongoDB.connectDB"(
+    	String databaseName	
+     , 	String collectionName	) {
+    (new MongoDB()).connectDB(
+        	databaseName
+         , 	collectionName)
+}
+
+
+def static "MongoDB.insertOpenCIF"(
+    	String databaseName	
+     , 	String collectionName	) {
+    (new MongoDB()).insertOpenCIF(
+        	databaseName
+         , 	collectionName)
+}
+
+
+def static "MongoDB.deleteOpenCIF"(
+    	String databaseName	
+     , 	String collectionName	) {
+    (new MongoDB()).deleteOpenCIF(
+        	databaseName
+         , 	collectionName)
+}
+
+
+def static "utilsKeywordPackage.UtilsKeyword.swipeDown"(
+    	int times	) {
+    (new utilsKeywordPackage.UtilsKeyword()).swipeDown(
+        	times)
+}
+
+
+def static "utilsKeywordPackage.UtilsKeyword.swpieRight"(
+    	int times	) {
+    (new utilsKeywordPackage.UtilsKeyword()).swpieRight(
+        	times)
+}
+
+
+def static "utilsKeywordPackage.UtilsKeyword.swipeDown_DropdownPenumpang"(
+    	int times	) {
+    (new utilsKeywordPackage.UtilsKeyword()).swipeDown_DropdownPenumpang(
+        	times)
+}
+
+
+def static "utilsKeywordPackage.UtilsKeyword.select_Provinsi"(
+    	String provinsi	) {
+    (new utilsKeywordPackage.UtilsKeyword()).select_Provinsi(
+        	provinsi)
+}
+
+
+def static "utilsKeywordPackage.UtilsKeyword.select_KotaKabupaten"(
+    	String kota_kabupaten	) {
+    (new utilsKeywordPackage.UtilsKeyword()).select_KotaKabupaten(
+        	kota_kabupaten)
+}
+
+
+def static "utilsKeywordPackage.UtilsKeyword.select_Kecamatan"(
+    	String kecamatan	) {
+    (new utilsKeywordPackage.UtilsKeyword()).select_Kecamatan(
+        	kecamatan)
+}
+
+
+def static "utilsKeywordPackage.UtilsKeyword.select_DesaKelurahann"(
+    	String desa_kelurahan	) {
+    (new utilsKeywordPackage.UtilsKeyword()).select_DesaKelurahann(
+        	desa_kelurahan)
+}
+
+
+def static "utilsKeywordPackage.UtilsKeyword.select_KodePos"(
+    	String kode_pos	) {
+    (new utilsKeywordPackage.UtilsKeyword()).select_KodePos(
+        	kode_pos)
+}
+
+
+def static "utilsKeywordPackage.UtilsKeyword.select_Pekerjaan"(
+    	String pedagang	) {
+    (new utilsKeywordPackage.UtilsKeyword()).select_Pekerjaan(
+        	pedagang)
+}
+
+
+def static "utilsKeywordPackage.UtilsKeyword.inputTanggalLahir"(
+    	String tanggalLahir	) {
+    (new utilsKeywordPackage.UtilsKeyword()).inputTanggalLahir(
+        	tanggalLahir)
+}
+
+
+def static "utilsKeywordPackage.UtilsKeyword.waitAfterInsertManualOTP_Registration"(
+    	int timeout	) {
+    (new utilsKeywordPackage.UtilsKeyword()).waitAfterInsertManualOTP_Registration(
+        	timeout)
+}
+
+
+def static "RegistrasiOnline.select_Provinsi"(
+    	String provinsi	) {
+    (new RegistrasiOnline()).select_Provinsi(
+        	provinsi)
+}
+
+
+def static "RegistrasiOnline.select_KotaKabupaten"(
+    	String kota_kabupaten	) {
+    (new RegistrasiOnline()).select_KotaKabupaten(
+        	kota_kabupaten)
+}
+
+
+def static "RegistrasiOnline.select_Kecamatan"(
+    	String kecamatan	) {
+    (new RegistrasiOnline()).select_Kecamatan(
+        	kecamatan)
+}
+
+
+def static "RegistrasiOnline.select_DesaKelurahann"(
+    	String desa_kelurahan	) {
+    (new RegistrasiOnline()).select_DesaKelurahann(
+        	desa_kelurahan)
+}
+
+
+def static "RegistrasiOnline.select_KodePos"(
+    	String kode_pos	) {
+    (new RegistrasiOnline()).select_KodePos(
+        	kode_pos)
+}
+
+
+def static "RegistrasiOnline.select_KantorCabangPadanan"(
+    	String kantorCabangPadanan	) {
+    (new RegistrasiOnline()).select_KantorCabangPadanan(
+        	kantorCabangPadanan)
+}
+
+
+def static "RegistrasiOnline.select_OutletBNITerdekat"(
+    	String outletBNITerdekat	) {
+    (new RegistrasiOnline()).select_OutletBNITerdekat(
+        	outletBNITerdekat)
+}
+
+
+def static "Ferizy.select_Layanan"(
+    	String layanan	) {
+    (new Ferizy()).select_Layanan(
+        	layanan)
+}
+
+
+def static "Ferizy.select_PenggunaJasa"(
+    	String penggunaJasa	) {
+    (new Ferizy()).select_PenggunaJasa(
+        	penggunaJasa)
+}
+
+
+def static "Ferizy.select_GolonganKendaraan"(
+    	String golonganKendaraan	) {
+    (new Ferizy()).select_GolonganKendaraan(
+        	golonganKendaraan)
+}
+
+
+def static "Ferizy.input_Jadwal"(
+    	String jadwal	) {
+    (new Ferizy()).input_Jadwal(
+        	jadwal)
+}
+
+
+def static "screenshots.fullPageScreenshots"() {
+    (new screenshots()).fullPageScreenshots()
+}
+
+
+def static "screenshots.startApplication"(
+    	String fileApk	) {
+    (new screenshots()).startApplication(
+        	fileApk)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.createExcelFile"(
+    	String filePath	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).createExcelFile(
+        	filePath)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.createExcelSheet"(
+    	Workbook workbook	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).createExcelSheet(
+        	workbook)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.createExcelSheet"(
+    	Workbook workbook	
+     , 	String sheetName	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).createExcelSheet(
+        	workbook
+         , 	sheetName)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.getSheetNames"(
+    	Workbook workbook	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).getSheetNames(
+        	workbook)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.createExcelSheets"(
+    	Workbook workbook	
+     , 	java.util.List<String> sheetNames	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).createExcelSheets(
+        	workbook
+         , 	sheetNames)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.getWorkbook"(
+    	String filePath	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).getWorkbook(
+        	filePath)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.createWorkbook"(
+    	String filePath	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).createWorkbook(
+        	filePath)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.saveWorkbook"(
+    	String filePath	
+     , 	Workbook workbook	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).saveWorkbook(
+        	filePath
+         , 	workbook)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.setValueToCellByIndex"(
+    	Sheet sheet	
+     , 	int rowIndex	
+     , 	int colIndex	
+     , 	Object value	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).setValueToCellByIndex(
+        	sheet
+         , 	rowIndex
+         , 	colIndex
+         , 	value)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.setValueToCellByAddress"(
+    	Sheet sheet	
+     , 	String cellAddress	
+     , 	Object value	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).setValueToCellByAddress(
+        	sheet
+         , 	cellAddress
+         , 	value)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.setValueToCellByAddresses"(
+    	Sheet sheet	
+     , 	Map content	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).setValueToCellByAddresses(
+        	sheet
+         , 	content)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.getExcelSheet"(
+    	Workbook wbs	
+     , 	String sheetName	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).getExcelSheet(
+        	wbs
+         , 	sheetName)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.getExcelSheet"(
+    	String filePath	
+     , 	int sheetIndex	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).getExcelSheet(
+        	filePath
+         , 	sheetIndex)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.getExcelSheet"(
+    	String filePath	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).getExcelSheet(
+        	filePath)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.getExcelSheet"(
+    	Workbook wbs	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).getExcelSheet(
+        	wbs)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.getExcelSheetByName"(
+    	String filePath	
+     , 	String sheetName	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).getExcelSheetByName(
+        	filePath
+         , 	sheetName)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.getCellByIndex"(
+    	Sheet sheet	
+     , 	int rowIdx	
+     , 	int colIdx	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).getCellByIndex(
+        	sheet
+         , 	rowIdx
+         , 	colIdx)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.getCellByAddress"(
+    	Sheet sheet	
+     , 	String cellAddress	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).getCellByAddress(
+        	sheet
+         , 	cellAddress)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.getCellValue"(
+    	Cell cell	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).getCellValue(
+        	cell)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.getCellValueByIndex"(
+    	Sheet sheet	
+     , 	int rowIdx	
+     , 	int colIdx	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).getCellValueByIndex(
+        	sheet
+         , 	rowIdx
+         , 	colIdx)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.getCellIndexByAddress"(
+    	Sheet sheet	
+     , 	String cellAddress	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).getCellIndexByAddress(
+        	sheet
+         , 	cellAddress)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.getCellValueByAddress"(
+    	Sheet sheet	
+     , 	String cellAddress	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).getCellValueByAddress(
+        	sheet
+         , 	cellAddress)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.getRowIndexByCellContent"(
+    	Sheet sheet	
+     , 	String cellContent	
+     , 	int columnIdxForCell	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).getRowIndexByCellContent(
+        	sheet
+         , 	cellContent
+         , 	columnIdxForCell)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.getTableContent"(
+    	Sheet sheet	
+     , 	int startRow	
+     , 	int endRow	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).getTableContent(
+        	sheet
+         , 	startRow
+         , 	endRow)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.getDataRows"(
+    	Sheet sheet	
+     , 	java.util.List<java.lang.Integer> rowIndexs	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).getDataRows(
+        	sheet
+         , 	rowIndexs)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.getMapOfKeyValueRows"(
+    	Sheet sheet	
+     , 	int keyRowIdx	
+     , 	int valueRowIdx	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).getMapOfKeyValueRows(
+        	sheet
+         , 	keyRowIdx
+         , 	valueRowIdx)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.getCellValueByRangeAddress"(
+    	Sheet sheet	
+     , 	String topLeftAddress	
+     , 	String rightBottomAddress	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).getCellValueByRangeAddress(
+        	sheet
+         , 	topLeftAddress
+         , 	rightBottomAddress)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.getColumnsByIndex"(
+    	Sheet sheet	
+     , 	java.util.List<java.lang.Integer> columnIndexes	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).getColumnsByIndex(
+        	sheet
+         , 	columnIndexes)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.compareTwoExcels"(
+    	Workbook workbook1	
+     , 	Workbook workbook2	
+     , 	boolean isValueOnly	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).compareTwoExcels(
+        	workbook1
+         , 	workbook2
+         , 	isValueOnly)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.compareTwoExcels"(
+    	Workbook workbook1	
+     , 	Workbook workbook2	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).compareTwoExcels(
+        	workbook1
+         , 	workbook2)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.compareTwoSheets"(
+    	Sheet sheet1	
+     , 	Sheet sheet2	
+     , 	boolean isValueOnly	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).compareTwoSheets(
+        	sheet1
+         , 	sheet2
+         , 	isValueOnly)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.compareTwoSheets"(
+    	Sheet sheet1	
+     , 	Sheet sheet2	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).compareTwoSheets(
+        	sheet1
+         , 	sheet2)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.compareTwoRows"(
+    	Row row1	
+     , 	Row row2	
+     , 	boolean isValueOnly	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).compareTwoRows(
+        	row1
+         , 	row2
+         , 	isValueOnly)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.compareTwoRows"(
+    	Row row1	
+     , 	Row row2	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).compareTwoRows(
+        	row1
+         , 	row2)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.compareTwoCells"(
+    	Cell cell1	
+     , 	Cell cell2	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).compareTwoCells(
+        	cell1
+         , 	cell2)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.compareTwoCells"(
+    	Cell cell1	
+     , 	Cell cell2	
+     , 	boolean isValueOnly	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).compareTwoCells(
+        	cell1
+         , 	cell2
+         , 	isValueOnly)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.getCellValuesByRangeIndexes"(
+    	Sheet sheet	
+     , 	int rowInd1	
+     , 	int colInd1	
+     , 	int rowInd2	
+     , 	int colInd2	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).getCellValuesByRangeIndexes(
+        	sheet
+         , 	rowInd1
+         , 	colInd1
+         , 	rowInd2
+         , 	colInd2)
+}
+
+
+def static "com.kms.katalon.keyword.excel.ExcelKeywords.locateCell"(
+    	Sheet sheet	
+     , 	Object cellContent	) {
+    (new com.kms.katalon.keyword.excel.ExcelKeywords()).locateCell(
+        	sheet
+         , 	cellContent)
+}
